6541858c64122d40610dcb6a61361ef6b94bf35f
grammik <47994756+grammik@users.noreply.github.com>
2019-02-26 00:50:35 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function($) {
+  $('#lightSlider').lightSlider({
+  item:1,
+  loop:true,
+  speed:500,
+  adaptiveHeight:true,
+  auto:true,
+  pauseOnHover: true,
+  pause:6000
+});
+});
---
+++ script.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+  $('#lightSlider').lightSlider();
+  item:1
+});
+$(document).ready(function() {
+    $('#adaptive').lightSlider({
+        adaptiveHeight:true,
+        item:1,
+        slideMargin:0,
+        loop:true
+    });
+});
